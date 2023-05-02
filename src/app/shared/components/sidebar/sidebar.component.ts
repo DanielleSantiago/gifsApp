90e4a003ef019f0ee4c7df2cbf79ad918ef12c6e
Danielle Santiago <dani.santiago89@hotmail.com>
2023-05-02 19:35:57 -0300

    Salvando as buscas em localStorage e recuperando o histórico de buscas, exibindo por default a primeira busca da lista

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
-import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
 
 @Component({
   selector: 'shared-sidebar',
